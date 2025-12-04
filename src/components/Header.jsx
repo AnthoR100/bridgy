415b27f2bda9f8c4726d460cef551cbea734cdf5
anthony hersant <anthony.hersant@microtec.fr>
2025-12-04 17:30:59 +0100

    Init: initial project setup with vite and base files

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+      <header>
+        <h1>Bridgy</h1>
+        <nav>
+          {/* Navigation à implémenter */}
+        </nav>
+      </header>
+    );
+  };
+  
+  export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+      <div>
+        <h1>Bienvenue sur Bridgy</h1>
+        <p>La plateforme qui connecte étudiants et entreprises</p>
+      </div>
+    );
+  };
+  
+  export default Home;
